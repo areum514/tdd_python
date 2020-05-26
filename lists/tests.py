9820c0fac327c2065dbb11399120b2be0bd72365
areum514 <21500514@handong.edu>
2020-05-26 17:37:42 +0900

    Add app for lists, with deliberately failing unit test and Basic view now returns minimal HTML

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,19 @@
+from django.urls import resolve
 from django.test import TestCase
-
+from django.http import HttpRequest
 # Create your tests here.
+from lists.views import home_page
 
 
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1+1, 3)
+class HomePageTest(TestCase):
+    def test_root_url_reverse_to_home_pabe_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        print(response)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
